27600767658316f752d5ef47c8b7a67e6b1b889a
moitavanhalen <arthurmiguelpenteado@gmail.com>
2025-10-01 20:31:15 -0300

    falencia
    desisti, estou comecando do zero acompanhando a correção

--- atividadesenac/src/main/java/com/mycompany/atividadesenac/atvsenac.java
+++ atividadesenac/src/main/java/com/mycompany/atividadesenac/atvsenac.java
@@ -1,16 +1,13 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package com.mycompany.atividadesenac;
 
 /**
  *
  * @author arthur.mpenteado
  */
-public class Atividadesenac {
-
-    public static void main(String[] args) {
-        
-    }
+public class atvsenac {
+    
 }
---
+++ cas/src/main/java/com/mycompany/cas/Cas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cas;
+
+/**
+ *
+ * @author arthur.mpenteado
+ */
+public class Cas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
